e3f2923238d84420653ccc6ae62a0652bff1e39c
Alex <aleksandr_1992_15@mail.ru>
2017-09-14 21:43:20 +0300

    2

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 $( document ).ready(function() {
-	$.post( "ajax/test.html", function( data ) {
-		$( "#demo" ).html( data );
+
+	$.post( "http://promo.fabiohome.ru/test", {begin: "2017/08/13", end: "2017/10/13"})
+	.done(function(date){
+
+		console.log("success");
 	});
+
+	console.log("hi");
 });
