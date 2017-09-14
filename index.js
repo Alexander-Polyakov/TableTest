d148e0c6e092d2ec5b3bb97af22bfa9c8bf8c76e
Alex <aleksandr_1992_15@mail.ru>
2017-09-15 00:41:24 +0300

    2

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 $( document ).ready(function() {
 
 
-	$.getJSON("http://promo.fabiohome.ru/test?callback=?", 'begin=2017/08/13&end=2017/10/13' ,function(json){
+	$.getJSON("https://cors.io/?http://promo.fabiohome.ru/test?format=json", 'begin=2017/08/13&end=2017/10/13' ,function(json){
 		console.log(json);
 	});
 });
+
+
 
 
 
