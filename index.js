33c961e3ce83b402aa6c64bf884fcb754027ad5b
Alex <aleksandr_1992_15@mail.ru>
2017-09-15 00:29:29 +0300

    2

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 $( document ).ready(function() {
 
-	$.post( "http://promo.fabiohome.ru/test", {begin: "2017/08/13", end: "2017/10/13"})
-	.done(function(date){
 
-		console.log("success");
+	$.getJSON("http://promo.fabiohome.ru/test?callback=?", 'begin=2017/08/13&end=2017/10/13' ,function(json){
+		console.log(json);
 	});
-
-	console.log("hi");
 });
+
+
+
